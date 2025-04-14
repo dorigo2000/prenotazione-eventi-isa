d436f410ecad94814ff29089ed1edf0d44ff7f1a
Marco Dorigo <marco.dorigo@edu.unife.it>
2025-04-14 16:48:09 +0200

    aggiunta nuovi features test User

---
+++ spec/features/user_subscribe_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.feature "UserSubscibe", type: :feature do
+  let(:partecipante) { create(:user) }
+  let(:organizzatore) { create(:user, tipo: "organizzatore") }
+  let!(:event) { create(:event, user: organizzatore, data_inizio: Date.tomorrow, data_fine: Date.tomorrow, orario_inizio: "10:00", orario_fine: "12:00", max_partecipanti: 10)}
+  let!(:event2) { create(:event, user: organizzatore, data_inizio: Date.tomorrow, data_fine: Date.tomorrow, orario_inizio: "15:00", orario_fine: "17:00", max_partecipanti: 10)}
+
+  before do
+    visit root_path
+    fill_in "Email", with: partecipante.email
+    fill_in "Password", with: partecipante.password
+    click_button "Accedi"
+    expect(current_path).to eq(events_path)
+  end
+
+  scenario "Partecipante si iscrive ad un evento" do
+    visit events_path
+
+    within("[data-event-id='#{event.id}']") do
+      click_button("Iscriviti")
+    end
+    expect(page).to have_content("Iscrizione effettuata con successo!")
+  end
+
+  scenario "Partecipante cancella l'iscrizione ad un evento" do
+    visit events_path
+    within("[data-event-id='#{event.id}']") do
+      click_button("Iscriviti")
+    end
+    expect(page).to have_content("Iscrizione effettuata con successo!")
+  
+    visit subscriptions_path
+    within("[data-event-id='#{event.id}']") do
+      click_button("Elimina")
+    end
+    expect(page).to have_content("La tua iscrizione è stata cancellata con successo!")
+  end
+
+  scenario "Partecipante non può iscriversi a due eventi nella stessa data" do
+    visit events_path 
+    click_button "Iscriviti", match: :first
+
+    visit events_path
+    within("[data-event-id='#{event2.id}']") do
+      click_button "Iscriviti"
+    end
+    expect(page).to have_content("Impossibile effettuare l'iscrizione, sei già iscritto a un evento nella stessa data!")
+  end
+end
